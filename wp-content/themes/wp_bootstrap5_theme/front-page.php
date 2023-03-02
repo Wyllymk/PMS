08297efffd9e93cbb8abdef6411cadfabe99813b
Kennedy-karuri <kennkaruri99@gmail.com>
2023-03-02 09:33:10 +0300

    created landing page

--- wp-content/themes/wp_bootstrap5_theme/front-page.php
+++ wp-content/themes/wp_bootstrap5_theme/front-page.php
@@ -6,10 +6,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="wp-content/themes/wp_bootstrap5_theme/assets/css/custom.css">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="wp-content/themes/wp_bootstrap5_theme/assets/css/custom.css">
+    <title>PMS</title>
+    
 </head>
 <body>
+
+
 <div class="homepage">
         <div class="homepage-heading">
             <div class="heading2">
@@ -18,28 +21,25 @@
                 </div>
                 <div class="links">
                     <li class="list"><a href="#">Home</a></li>
-                    <li class="list"><a href="#">About</a></li>
-                    <li class="list"><a href="#">Services</a></li>
+                    <li class="list"><a href="#">Product</a></li>
+                    <li class="list"><a href="#">Pricing</a></li>
                     <li class="list"><a href="#">Contact</a></li>
                 </div>
             </div>
             <div class="member">
-                <li class="list"><a href="#">Login</a></li>
-                <button>Register</button>
+                 <button><li class="list"><a href="#">Login</a></li></button>
             </div>
         </div>
 
         <div class="homepage-content">
-            <h1>PROJECT MANAGEMENT SYSTEM</h1>
-            <p>We Offer high quality Work and Project management techniques</p>
+            <h1>Effortlessly manage Your Projects with our Powerful Project Management System.</h1>
             <div class="buttons">
                 <button class="quote">Login</button>
-                <button class="learn">Contact Us</button>
+                <button class="learn">Contact-Us</button>
             </div>
         </div>
     </div>
 
-
-
+    
 
 <?php get_footer();?>
