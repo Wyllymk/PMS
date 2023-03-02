0690576ae15c4074489a1183515a6f12cf424d56
Kennedy-karuri <kennkaruri99@gmail.com>
2023-03-02 14:35:33 +0300

    styled contact form

--- wp-content/themes/wp_bootstrap5_theme/footer.php
+++ wp-content/themes/wp_bootstrap5_theme/footer.php
@@ -1,5 +1,5 @@
-        <footer>
-        <footer class="bg-light text-center text-lg-start">
+
+        <footer class="bg-light text-center text-lg-start" style="padding-top:50px;">
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
     © 2023 Copyright:
--- wp-content/themes/wp_bootstrap5_theme/front-page.php
+++ wp-content/themes/wp_bootstrap5_theme/front-page.php
@@ -3,82 +3,131 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="wp-content/themes/wp_bootstrap5_theme/assets/css/custom.css">
-    <title>PMS</title>
-    
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="wp-content/themes/wp_bootstrap5_theme/assets/css/custom.css">
+<title>PMS</title>
+
 </head>
 <body>
 
 
 <div class="homepage">
-        <div class="homepage-heading">
-            <div class="heading2">
-                <div class="brand">
-                    <li class="list">PROJECT MANAGEMENT SYSTEM</li>
+    <div class="homepage-heading">
+        <div class="heading2">
+            <div class="brand">
+                <li class="list">PROJECT MANAGEMENT SYSTEM</li>
+            </div>
+            <div class="links">
+                <li class="list"><a href="#">Home</a></li>
+                <li class="list"><a href="#">Product</a></li>
+                <li class="list"><a href="#">Pricing</a></li>
+                <li class="list"><a href="#">Contact</a></li>
+            </div>
+        </div>
+        <div class="member">
+                <button><li class="list"><a href="#">Login</a></li></button>
+        </div>
+    </div>
+
+    <div class="homepage-content">
+        <h1>Effortlessly manage Your Projects with our Powerful Project Management System.</h1>
+        <div class="buttons">
+            <button class="login">Login</button>
+            <button class="contact">Contact-Us</button>
+        </div>
+    </div>
+</div>
+
+<div class="second-page">
+    <h1>Offered Service</h1>
+    <p>Effortlessly Manage Your Projects With Our Powerful Project Management System</p>
+    <p>Assign Tasks, Track Progress and Streamline Workflows With Ease</p>
+
+</div>
+
+
+<div class="contact-page">
+    <h1>CONTACT DETAILS</h1>
+    <P>For any type of inquiries please dont hesitate to ge intouch with us.<br> The easiest way is to send us an email to the following adress or use the form below.</P>
+    <div class="row justify-content-center">
+    <div class="row ">
+    <div class="col-lg-7 mx-auto">
+    <div class="card mt-2 mx-auto p-4 bg-light">
+        <div class="card-body bg-light">
+    
+        <div class = "container" style="padding-bottom:10px;">
+        <form id="contact-form" role="form" method="post">
+            <div class="controls">
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="form_name">Firstname *</label>
+                        <input id="form_name" type="text" name="fullname" class="form-control" placeholder="Please enter your full name *" required="required" data-error="Firstname is required.">
+                        
+                    </div>
                 </div>
-                <div class="links">
-                    <li class="list"><a href="#">Home</a></li>
-                    <li class="list"><a href="#">Product</a></li>
-                    <li class="list"><a href="#">Pricing</a></li>
-                    <li class="list"><a href="#">Contact</a></li>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="form_lastname">Email *</label>
+                        <input id="form_lastname" type="text" name="email" class="form-control" placeholder="Please enter your email *" required="required" data-error="Lastname is required.">
+                    </div>
                 </div>
             </div>
-            <div class="member">
-                 <button><li class="list"><a href="#">Login</a></li></button>
+            <div class="row">
+                
+                    <div class="form-group">
+                        <label for="form_email">Telephone *</label>
+                        <input id="form_email" type="number" name="telephone" class="form-control" placeholder="Please enter your Number *" required="required" data-error="Valid email is required.">
+                        
+                </div>
             </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label for="form_message">Message *</label>
+                        <textarea id="form_message" name="message" class="form-control" placeholder="Write your message here." rows="4" required="required" data-error="Please, leave us a message."></textarea
+                            >
+                        </div>
+
+                    </div>
+
+
+                <div class="col-md-12">
+                    <div class="reg-btn" style="padding-top:20px;">
+                    <input type="submit" value="Register" name="submitbtn" class="btn btn-primary btn-block">
+                    </div>           
+            
+        </div>
+                
+            </div>
+        
+            </div>
+
+
+    </div>
+        </form>
+    </div>
         </div>
 
-        <div class="homepage-content">
-            <h1>Effortlessly manage Your Projects with our Powerful Project Management System.</h1>
-            <div class="buttons">
-                <button class="login">Login</button>
-                <button class="contact">Contact-Us</button>
-            </div>
-        </div>
-    </div>
 
-    <div class="second-page">
-        <h1>Offered Service</h1>
-        <p>Effortlessly Manage Your Projects With Our Powerful Project Management System</p>
-        <p>Assign Tasks, Track Progress and Streamline Workflows With Ease</p>
-
-    </div>
-
-
-    <div class="contact-page">
-        <h1>CONTACT DETAILS</h1>
-        <P>FOr any type of inquiries please dont hesitate to ge intouch with us.<br> The easiest way is to send us an email to the following adress or use the form below.</P>
-        <div class="row justify-content-center">
+</div>
     
 
-   
+</div>
 
-    
-        <form action="" method="post">
-        <div class="form-group">
-            <input type="text" name="fullname" class="form-control input-sm mb-3" id="fullname" placeholder="input your full names here" required>
-        </div>
-        <div class="form-group">
-            <input type="text" name="email" class="form-control input-sm mb-3" id="email" placeholder="input your email here" required>
-        </div>
-        <div class="form-group">
-            <input type="number" name="telephone" class="form-control input-sm mb-3" id="telephone" placeholder="input your telephone number here" required>
-        </div>
-        <div class="form-group">
-            <input type="text" name="message" class="form-control input-sm mb-3" id="message" placeholder="input your message here" required>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-xs-4 col-sm-4 col-md-4">
-                <input type="submit" value="Register" name="submitbtn" class="btn btn-primary btn-block">
-            </div>
-        </div>
-    </form>
- </div>
-    </div>
 
-    
+</div>
+</div>
+
+
+
+
+</div>
+</div>
+
+
 
 <?php get_footer();?>
